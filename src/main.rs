154c5432bc67bb996941ebe2160bdc8b220f79c0
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-19 21:50:15 -0800

    Complete problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod problem1 {
+	struct Multiples {
+		next: i32,
+		of: Vec<i32>
+	}
+
+	impl Iterator for Multiples {
+		type Item = i32;
+
+		fn next(&mut self) -> Option<i32> {
+			let mut next = None;
+			while next.is_none() {
+				if self.of.iter().any(|o| self.next % o == 0) {
+					next = Some(self.next);
+				}
+
+				self.next += 1
+			}
+
+			next
+		}
+	}
+
+	fn get_multiples(of: Vec<i32>) -> Multiples {
+		Multiples { next: *of.iter().min().unwrap(), of: of }
+	}
+
+	pub fn get_answer() -> i32 {
+		get_multiples(vec![3, 5]).take_while(|&x| x < 1000).fold(0, |s, i| s + i)
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Problem 1: {}", problem1::get_answer());
 }
