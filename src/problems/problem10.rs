ec25d95baea8789e212f40b50e0995fa80b1b54a
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-20 15:55:01 -0800

    Complete problem 10

--- src/problems/problem10.rs
+++ src/problems/problem10.rs
@@ -2,5 +2,5 @@ use support::sieve::SieveOfEratosthenes;
 
 #[allow(dead_code)]
 pub fn get_answer() -> u64 {
-	SieveOfEratosthenes::iter().take(10001).last().unwrap()
+    SieveOfEratosthenes::get_primes_up_to(2000000).iter().fold(0, |s, p| s + p)
 }
--- src/support/sieve.rs
+++ src/support/sieve.rs
@@ -1,17 +1,46 @@
 pub struct SieveOfEratosthenes {
-	next: i32,
-	so_far: Vec<i32>,
+	next: u64,
+	so_far: Vec<u64>,
+}
+
+fn get_next_prime(prime: u64, list: &Vec<u64>) -> Option<u64> {
+	let option = list.iter().skip_while(|&p| *p <= prime).skip_while(|&p| *p == 0).nth(0);
+	if option.is_some() {
+		Some(*option.unwrap())
+	} else {
+		None
+	}
 }
 
 impl SieveOfEratosthenes {
-	pub fn start() -> SieveOfEratosthenes {
+	pub fn iter() -> SieveOfEratosthenes {
 		SieveOfEratosthenes { next: 2, so_far: Vec::new() }
+	}
+
+	pub fn get_primes_up_to(n: u64) -> Vec<u64> {
+		let mut list: Vec<u64> = (2..n + 1).collect();
+		let mut prime = 1;
+		loop {
+			let prime_option = get_next_prime(prime, &list);
+			if prime * prime > n || prime_option.is_none() {
+				break;
+			}
+
+			prime = prime_option.unwrap();
+			let mut index = ((2 * prime) - 2) as usize;
+			while index < list.len() {
+				list[index] = 0;
+				index += prime as usize;
+			}
+		}
+
+		list.iter().filter(|&p| *p != 0).map(|&p| p).collect()
 	}
 }
 
 impl Iterator for SieveOfEratosthenes {
-	type Item = i32;
-	fn next(&mut self) -> Option<i32> {
+	type Item = u64;
+	fn next(&mut self) -> Option<u64> {
 		let mut next = None;
 		while next.is_none() {
 			if !self.so_far.iter().any(|&x| self.next % x == 0) {
