8de498ebf671741fb5a65050d9bd5ec047970561
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-19 21:58:54 -0800

    Complete problem 2

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,33 @@ mod problem1 {
 	}
 }
 
+mod problem2 {
+	struct Fibonacci {
+		previous: i32,
+		current: i32,
+	}
+
+	impl Iterator for Fibonacci {
+		type Item = i32;
+
+		fn next(&mut self) -> Option<i32> {
+			let next = self.previous + self.current;
+			self.previous = self.current;
+			self.current = next;
+			Some(next)
+		}
+	}
+
+	fn get_fibonacci() -> Fibonacci {
+		Fibonacci { previous: 0, current: 1 }
+	}
+
+	pub fn get_answer() -> i32 {
+		get_fibonacci().take_while(|&x| x <= 4000000).filter(|&x| x % 2 == 0).fold(0, |s, i| s + i)
+	}
+}
+
 fn main() {
     println!("Problem 1: {}", problem1::get_answer());
+    println!("Problem 2: {}", problem2::get_answer());
 }
