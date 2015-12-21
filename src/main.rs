3badfec7befa2d98cf60f6bfd3725946f87b7e1c
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-20 18:22:37 -0800

    Complete problem 13

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod support;
 mod problems;
-use problems::problem12::get_answer;
+extern crate num;
+
+use problems::problem13::get_answer;
+
+
 
 fn main() {
     println!("Problem: {}", get_answer());
