44d268eb73c3179413ef4f3444c8768e91e98d51
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-25 17:16:14 -0800

    Complete problem 21

---
+++ src/problems/problem21.rs
@@ -0,0 +1,15 @@
+use support::factors::Factors;
+
+fn d(n: u64) -> u64 {
+	Factors::iter(n).fold(0, |s, v| s + v) - n
+}
+
+fn is_amicable(n: u64) -> bool {
+	let other = d(n);
+	other != n && n == d(other) 
+}
+
+#[allow(dead_code)]
+pub fn get_answer() -> u64 {
+	(1..10000).filter(|&v| is_amicable(v)).fold(0, |s, v| s + v)
+}
