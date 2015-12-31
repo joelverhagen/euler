6a2d6b8133bb6b525ad58a4dceba50b0d7537af2
Joel Verhagen <jver@microsoft.com>
2015-12-31 12:01:27 -0800

    Complete problem 30

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate num;
 extern crate chrono;
 extern crate permutohedron;
 
-use problems::problem29::get_answer;
+use problems::problem30::get_answer;
 
 fn main() {
     println!("Problem: {}", get_answer());
---
+++ src/problems/problem30.rs
@@ -0,0 +1,35 @@
+use num::bigint::BigUint;
+use num::pow;
+use num::FromPrimitive;
+use support::digits::HasDigits;
+use std::collections::HashSet;
+
+fn get_max(p: usize) -> u64 {
+	let mut digits = 2;
+	let mut max = 0;
+	loop {
+		let new_max = digits as u64 * pow(9u64, p);
+		if new_max.iter_digits().count() < digits {
+			break;
+		}
+
+		max = new_max;
+		digits += 1;
+	}
+
+	max
+}
+
+#[allow(dead_code)]
+pub fn get_answer() -> u64 {
+	let p = 5;
+	let mut sum = 0;
+	for i in 10..get_max(p) + 1 {
+		let maybe_i = i.iter_digits().fold(0, |s, d| s + pow(d as u64, p));
+		if i == maybe_i {
+			sum += i;
+		}
+	}
+
+	sum
+}
